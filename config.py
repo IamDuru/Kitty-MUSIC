fb19f3fc6dcb4a9124a80f785a29f8f75be9f041
Official Durgesh <durgeshgokhale656@gmail.com>
2024-09-04 10:50:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/OpAlex07/Kitty-MUSIC",
+    "https://github.com/IamDuru/Kitty-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
